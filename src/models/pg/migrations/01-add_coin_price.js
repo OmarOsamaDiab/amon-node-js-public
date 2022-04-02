87c4baf5d1ea57b6d0aeb1afb5f5e2fe70fc4a09
omar osama <omarosamadiab10@gmail.com>
2022-04-02 19:02:10 +0200

    add price and last updated price to  coin table

---
+++ src/models/pg/migrations/01-add_coin_price.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: async function (query, transaction) {
+    const sql = `
+      ALTER TABLE "Coin" 
+      ADD "price" VARCHAR(255),
+      ADD "priceLastUpdatedAt" DATE;
+    `
+    await transaction.sequelize.query(sql, { raw: true, transaction });
+  },
+
+  down: async function (query, transaction) {
+    const sql = `
+     ALTER TABLE "Coin"
+     DROP "price",
+     DROP "priceLastUpdatedAt";
+   `
+    await transaction.sequelize.query(sql, { raw: true, transaction });
+  },
+};
